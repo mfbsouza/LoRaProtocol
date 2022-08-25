d94958e5303604afc475c2154f5e8c83971e0252
mfbsouza <mfbsouza.it@gmail.com>
2022-08-25 14:14:52 -0300

    python script for quick testing Radioenge's LoRaMESH
    Signed-off-by: mfbsouza <mfbsouza.it@gmail.com>

---
+++ scripts/test_loramesh.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import sys
+import serial
+from time import sleep
+
+def main():
+    if len(sys.argv) < 2:
+        print("Error: expected more command line arguments")
+        print("Syntax: %s </dev/serial_port>" %sys.argv[0])
+        exit(1)
+
+    # 'Local Read' Frame acording to the datasheet
+    msg = b'\x00\x00\xE2\x00\x00\x00\x15\xB8'
+
+    # Open the serial port
+    lora = serial.Serial(sys.argv[1], 9600, timeout=1)
+
+    # Send the request through the serial port and wait
+    # the read the serial port and print it out
+    lora.write(msg)
+    sleep(1)
+    recv = lora.read_all()
+    print(recv)
+
+    # close the serial port
+    lora.close()
+
+    exit(0)
+
+if __name__ == '__main__':
+    main()
