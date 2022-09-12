4fbfa3dee673054a5b73c668ac3314e32aa5381e
mfbsouza <mfbsouza.it@gmail.com>
2022-09-11 23:15:48 -0300

    scan ok, connect almost ok
    Signed-off-by: mfbsouza <mfbsouza.it@gmail.com>

--- src/LoRaPCL/LoRaPCL.c
+++ src/LoRaPCL/LoRaPCL.c
@@ -7,18 +7,22 @@
 enum lorapcl_states {
 	NO_MODE,
 	DISCONNECTED,
-	SEND_SCAN,
-	WAIT_GW_ID,
-	BUILD_GW_IDS,
-	INFO_SCAN_FAIL,
-	INFO_SCAN_OK,
+
 	IDLE,
-	SEND_GW_ID
+	SEND_GW_ID,
+	CHECK_FREE_NID,
+	SEND_SYNC_SET_ID,
+	WAIT_ACK,
+	UPDATE_CLIENT_LIST
 };
 
 typedef enum {
 	SCAN,
-	GW_INFO
+	GW_INFO,
+	SYNC,
+	SYNC_SET_ID,
+	ACK,
+	GW_FULL
 } Packet_Type_t;
 
 typedef struct {
@@ -40,14 +44,12 @@ static const LoraInterface_t *radio = NULL;
 static const TimerInterface_t *mTimer = NULL;
 static Packet_Frame_t buffer;
 
-static uint8_t gw_nid, gw_uid;
-static uint8_t app_informed = 0;
-
 /* private functions */
 static Packet_Type_t get_packet_type(Packet_Frame_t *packet);
+static void build_signal(uint8_t s_uid, uint8_t s_nid, uint8_t d_uid, uint8_t d_nid, uint8_t flag);
 static void build_response(Packet_Frame_t *packet, uint8_t flags, uint8_t seq_number);
 
-LoRa_Error_t init_protocol(
+LoRa_Error_t lorapcl_init(
 		uint8_t uid,
 		LoRa_Mode_t op_mode,
 		const LoraInterface_t *lora_driver,
@@ -90,47 +92,22 @@ LoRa_Error_t init_protocol(
 	return NO_ERROR;
 }
 
-LoRa_Error_t scan_gateways()
+void lorapcl_gateway_fsm()
 {
-	if (mode == NONE) return NO_MODE_DEFINED;
-	if (mode == GATEWAY) return WRONG_MODE;
-	if (state != DISCONNECTED) return ERROR;
-	state = SEND_SCAN;
-	return NO_ERROR;
-}
-
-LoRa_Error_t lorapcl_status()
-{
-	if (state == INFO_SCAN_OK) {
-		app_informed = 1;
-		return NO_ERROR;
-	}
-	if (state == INFO_SCAN_FAIL) {
-		app_informed = 1;
-		return ERROR;
-	}
-	return PROCESSING;
-}
-
-void lorapcl_fsm()
-{
-	static unsigned long time_ref;
-	static uint8_t timeout_count = 0;
+	static uint8_t free_nid, old_nid, timeout_count = 0;
+	static unsigned long timer_ref;
 	uint8_t recv_nid, pkt_size;
-	Packet_Type_t msg_type;
 
 	switch (state) {
-	case NO_MODE:
-		break;
-	case DISCONNECTED:
-		break;
 	case IDLE:
 		if (radio->packet_available()) {
 			radio->packet_recv(&recv_nid, (uint8_t *)&buffer, &pkt_size);
-			msg_type = get_packet_type(&buffer);
 
-			if (msg_type == SCAN) {
+			if (get_packet_type(&buffer) == SCAN) {
 				state = SEND_GW_ID;
+			}
+			else if (get_packet_type(&buffer) == SYNC) {
+				state = CHECK_FREE_NID;
 			}
 		}
 		break;
@@ -141,61 +118,139 @@ void lorapcl_fsm()
 		state = IDLE;
 		break;
 	
-	case SEND_SCAN:
-		buffer.src_uid = unique_id;
-		buffer.src_nid = net_id;
-		buffer.dest_uid = 0X00;
-		buffer.dest_nid = 0x00;
-		buffer.flags_n_seq = ((SCAN << 4)&0xF0);
-		buffer.data_size = 0;
-		radio->packet_send(buffer.dest_nid, (uint8_t *)&buffer, (buffer.data_size + 6));
-		time_ref = mTimer->millis();
-		state = WAIT_GW_ID;
+	case CHECK_FREE_NID:
+		// TODO: do a real checking
+		free_nid = 0x01;
+		state = SEND_SYNC_SET_ID;
 		break;
-	
-	case WAIT_GW_ID:
-		if ((mTimer->millis() - time_ref) <= 5000) {
+
+	case SEND_SYNC_SET_ID:
+		old_nid = buffer.src_nid;
+		build_signal(unique_id, net_id, buffer.src_uid, free_nid, SYNC_SET_ID);
+		radio->packet_send(old_nid, (uint8_t *)&buffer, (buffer.data_size + 6));
+		timer_ref = mTimer->millis();
+		state = WAIT_ACK;
+		break;
+
+	case WAIT_ACK:
+		if((mTimer->millis() - timer_ref) <= 5000) {
 			if (radio->packet_available()) {
 				radio->packet_recv(&recv_nid, (uint8_t *)&buffer, &pkt_size);
-				msg_type = get_packet_type(&buffer);
-				if (msg_type == GW_INFO) {
-					state = BUILD_GW_IDS;
-					break;
+
+				if (get_packet_type(&buffer) == ACK) {
+					// Connection Done
+					state = UPDATE_CLIENT_LIST;
 				}
 			}
 		}
 		else {
 			timeout_count++;
 			if (timeout_count < 2) {
-				state = SEND_SCAN;
+				state = SEND_SYNC_SET_ID;
 			}
 			else if (timeout_count == 2) {
 				timeout_count = 0;
-				state = INFO_SCAN_FAIL;
+				state = IDLE;
 			}
 		}
 		break;
 	
-	case BUILD_GW_IDS:
-		gw_nid = buffer.src_nid;
-		gw_uid = buffer.src_uid;
-		state = INFO_SCAN_OK;
-		break;
-
-	case INFO_SCAN_OK:
-		if (app_informed) {
-			app_informed = 0;
-			state = DISCONNECTED;
-		}
-		break;
-
-	case INFO_SCAN_FAIL:
-		if (app_informed) {
-			app_informed = 0;
-			state = DISCONNECTED;
-		}
+	case UPDATE_CLIENT_LIST:
+		// TODO: inform all also
+		state = IDLE;
 		break;
 	}
+}
+
+LoRa_Error_t lorapcl_scan_gateway(uint8_t *gw_uid, uint8_t *gw_nid)
+{
+	uint8_t recv_nid, pkt_size, timeout_count = 0;
+	unsigned long timer_ref;
+
+	// check states
+	if (mode != NODE) return WRONG_MODE;
+	if (state != DISCONNECTED) return PCL_ERROR; // TODO: maybe create a NODE state var
+
+SEND_SCAN:
+	// build SCAN packet
+	build_signal(unique_id, net_id, 0x00, 0x00, SCAN);
+
+	// Send Scan Signal
+	radio->packet_send(buffer.dest_nid, (uint8_t *)&buffer, (buffer.data_size + 6));
+	timer_ref = mTimer->millis();
+
+WAIT_GW_ID:
+	if ((mTimer->millis() - timer_ref) <= 5000) {
+		if (radio->packet_available()) {
+			radio->packet_recv(&recv_nid, (uint8_t *)&buffer, &pkt_size);
+
+			if (get_packet_type(&buffer) == GW_INFO) {
+				*gw_uid = buffer.src_uid;
+				*gw_nid = buffer.src_nid;
+				return NO_ERROR;
+			}
+		}
+		goto WAIT_GW_ID;
+	}
+	else {
+		timeout_count++;
+		if (timeout_count < 2) goto SEND_SCAN;
+		else if (timeout_count == 2) return NO_RESPONSE;
+	}
+
+	return NOT_EXPECTED;
+}
+
+LoRa_Error_t lorapcl_connect(uint8_t gateway_uid, uint8_t gateway_nid)
+{
+	uint8_t recv_nid, pkt_size, timeout_count = 0;
+	unsigned long timer_ref;
+
+	// check states
+	if (mode != NODE) return WRONG_MODE;
+	// TODO: maybe create a NODE state var
+	if (state != DISCONNECTED) return PCL_ERROR;
+
+SEND_SYNC:
+	// build SYNC packet
+	build_signal(unique_id, net_id, gateway_uid, gateway_nid, SYNC);
+
+	// Send Sync Signal
+	radio->packet_send(buffer.dest_nid, (uint8_t *)&buffer, 6);
+	timer_ref = mTimer->millis();
+
+WAIT_NEW_ID:
+	if ((mTimer->millis() - timer_ref) <= 5000) {
+		if (radio->packet_available()) {
+			radio->packet_recv(&recv_nid, (uint8_t *)&buffer, &pkt_size);
+
+			if (get_packet_type(&buffer) == SYNC_SET_ID) {
+				// update to new NID
+				net_id = buffer.dest_nid;
+				radio->set_id(net_id);
+
+				// Send Ack to Gateway
+				build_signal(unique_id, net_id, gateway_uid, gateway_nid, ACK);
+				radio->packet_send(buffer.dest_nid, (uint8_t *)&buffer, 6);
+
+				// Connection Success
+				return NO_ERROR;
+			}
+			else if (get_packet_type(&buffer) == GW_FULL) {
+				// Connection Failed: Gateway Full
+				return GATEWAY_FULL;
+			}
+		}
+		goto WAIT_NEW_ID;
+	}
+	else {
+		timeout_count++;
+		if (timeout_count < 2) goto SEND_SYNC;
+		// Connection Failed: No Response From Gateway
+		else if (timeout_count == 2) return NO_RESPONSE;
+	}
+
+	return NOT_EXPECTED;
 }
 
 static Packet_Type_t get_packet_type(Packet_Frame_t *packet)
@@ -203,6 +258,16 @@ static Packet_Type_t get_packet_type(Packet_Frame_t *packet)
 	uint8_t flags;
 	flags = (packet->flags_n_seq >> 4)&0x0F;
 	return (Packet_Type_t)flags;
+}
+
+static void build_signal(uint8_t s_uid, uint8_t s_nid, uint8_t d_uid, uint8_t d_nid, uint8_t flag)
+{
+	buffer.src_uid = s_uid;
+	buffer.src_nid = s_nid;
+	buffer.dest_uid = d_uid;
+	buffer.dest_nid = d_nid;
+	buffer.flags_n_seq = ((flag << 4)&0xF0);
+	buffer.data_size = 0;
 }
 
 static void build_response(Packet_Frame_t *packet, uint8_t flags, uint8_t seq_number)
--- src/LoRaPCL/LoRaPCL.h
+++ src/LoRaPCL/LoRaPCL.h
@@ -17,23 +17,24 @@ typedef enum {
 
 typedef enum {
 	NO_ERROR,
-	ERROR,
-	PROCESSING,
+	PCL_ERROR,
 	NO_RESPONSE,
+	GATEWAY_FULL,
+	NOT_EXPECTED,
 	NO_MODE_DEFINED,
 	WRONG_MODE
 } LoRa_Error_t;
 
-LoRa_Error_t init_protocol(
+LoRa_Error_t lorapcl_init(
 		uint8_t uid,
 		LoRa_Mode_t op_mode,
 		const LoraInterface_t *lora_driver,
 		const TimerInterface_t *timer
 		);
 
-void lorapcl_fsm();
-LoRa_Error_t lorapcl_status();
-LoRa_Error_t scan_gateways();
+LoRa_Error_t lorapcl_scan_gateway  (uint8_t *gw_uid, uint8_t *gw_nid);
+LoRa_Error_t lorapcl_connect       (uint8_t gateway_uid, uint8_t gateway_nid);
+void         lorapcl_gateway_fsm   ();
 
 #ifdef __cplusplus
 }
